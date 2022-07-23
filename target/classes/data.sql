53d62d270511c1438f1ae6a41fe2e8e10b088d61
Joney Pereira <joney.pereira.ext@conductor.com.br>
2022-07-23 09:50:29 -0300

    Configuração da base de dados

---
+++ target/classes/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Jose da Silva', '06296453027', 1200.00 , TIMESTAMP WITH TIME ZONE '1980-07-13T20:50:07.12345Z',2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Maria da Silva', '01491520000', 1600.00 , TIMESTAMP WITH TIME ZONE '1981-04-13T18:50:07.12345Z',1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Pedro Paulo Gomes', '46756077058', 3600.00 , TIMESTAMP WITH TIME ZONE '1979-04-13T07:50:07.12345Z',3);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Joao Paulo Silva', '11066747075', 2100.00 , TIMESTAMP WITH TIME ZONE '1963-04-13T05:50:07.12345Z',2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Juliana Silva Mendes', '30410375055', 4700.00 , TIMESTAMP WITH TIME ZONE '1951-04-13T12:50:07.12345Z',6);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Fabiano de Sousa', '53695981008', 1500.00 , TIMESTAMP WITH TIME ZONE '2005-04-13T00:50:07.12345Z',5);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Maria Antonia Silva', '43065242052', 3200.00 , TIMESTAMP WITH TIME ZONE '2000-04-13T13:50:07.12345Z',1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Marcos Silva', '24558866038', 6700.00 , TIMESTAMP WITH TIME ZONE '1999-04-13T15:50:07.12345Z',0);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Tomaz de Arquino', '61407182013', 800.00 , TIMESTAMP WITH TIME ZONE '1972-04-13T21:50:07.12345Z',1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Pedro Paulo Sabino', '33112116003', 900.00 , TIMESTAMP WITH TIME ZONE '1989-04-13T22:50:07.12345Z',4);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Jose Gomes', '03442375088', 1000.00 , TIMESTAMP WITH TIME ZONE '1998-04-13T19:50:07.12345Z',6);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Arthur Valerio', '13433597081', 1200.00 , TIMESTAMP WITH TIME ZONE '1977-04-13T07:50:07.12345Z',2);
