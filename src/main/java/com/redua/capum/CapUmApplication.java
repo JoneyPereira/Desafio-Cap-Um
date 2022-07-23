48788c16d1c9013862b94a985b9226ceb129b5aa
Joney Pereira <joney.pereira.ext@conductor.com.br>
2022-07-23 05:46:03 -0300

    Criado o projeto

---
+++ src/main/java/com/redua/capum/CapUmApplication.java
@@ -0,0 +1,13 @@
+package com.redua.capum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapUmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapUmApplication.class, args);
+	}
+
+}
